361c255cce644fbbaaac43724aebbe74679a336a
MarkelGaming <105814034+MarkelGaming@users.noreply.github.com>
2022-05-26 18:25:48 +0500

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,60 +1,31 @@
 import java.util.Scanner;
- 
 public class Calculator {
-    static Scanner scanner = new Scanner(System.in);
- 
-    public static void main(String[] args) {
-        int num1 = getInt();
-        int num2 = getInt();
-        char operation = getOperation();
-        int result = calc(num1,num2,operation);
-        System.out.println("Результат операции: "+result);
-    }
- 
-    public static int getInt(){
-        System.out.println("Введите число:");
-        int num;
-        if(scanner.hasNextInt()){
-            num = scanner.nextInt();
-        } else {
-            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
-            scanner.next();
-            num = getInt();
-        }
-        return num;
-    }
- 
-    public static char getOperation(){
-        System.out.println("Введите операцию:");
-        char operation;
-        if(scanner.hasNext()){
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
-            scanner.next();
-            operation = getOperation();
-        }
-        return operation;
-    }
- 
-    public static int calc(int num1, int num2, char operation){
-        int result;
-        switch (operation){
-            case '+':
-                result = num1+num2;
-                break;
-            case '-':
-                result = num1-num2;
-                break;
-            case '*':
-                result = num1*num2;
-                break;
-            case '/':
-                result = num1/num2;
-                break;
-            default:
-                System.out.println("Операция не распознана. Повторите ввод.");
-                result = calc(num1, num2, getOperation());
-        return result;
-    }
+	
+	
+
+	public static void main(String[] args) {
+		      int num1;
+		      int num2;
+		      int ans;
+		      char op;
+		      Scanner scan = new Scanner(System.in);
+		      System.out.print("Please word your the equation : ");
+		      num1 = scan.nextInt();
+		      op = scan.next().charAt(0);
+		      num2 = scan.nextInt();
+		      switch(op) {
+		         case '+': ans = num1 + num2;
+		            break;
+		         case '-': ans = num1 - num2;
+		            break;
+		         case '*': ans = num1 * num2;
+		            break;
+		         case '/': ans = num1 / num2;
+		            break;
+		         default:  System.out.printf("Error! Enter correct operator");
+		            return;
+		      }
+		      System.out.print("\nThe result is your the equation :");
+		      System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+	}
 }
